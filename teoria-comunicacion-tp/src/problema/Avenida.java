81579f83614f7c47f3de193243b91850decca3d3
Nahuel Tori <nahueltori@gmail.com>
2014-07-19 20:31:59 -0300

    Update Avenida.java

--- teoria-comunicacion-tp/src/problema/Avenida.java
+++ teoria-comunicacion-tp/src/problema/Avenida.java
@@ -6,9 +6,18 @@ public class Avenida {
 
 	private List<Semaforo> listaSemaforos;
 	
-	private Trafico trafico;
+	private List<Trafico> listaTraficos;
 
-	private Evolucion calculos;
+
+	public Avenida(){
+		//TODO: Crear tramos de trafico y unirlos
+		//TODO: Crear semaforos en los tramos deseados
+	}
 	
-	
+	/**
+	 * Método que se encarga de generar aleatoriamente tráfico, dependiendo del horario del dìa.
+	 */
+	private void crearTraficoAleatoriamente(Date hora){
+		
+	}
 }
