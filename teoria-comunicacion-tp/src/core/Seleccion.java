b5a21cef97c55d0a74f3ea36ca994943b0550c7b
tapiajimena <jimena.soledad.tapia@gmail.com>
2014-07-26 23:45:51 -0300

    Listo TODO's de Seleccion.java. Resta prueba integral

--- teoria-comunicacion-tp/src/core/Seleccion.java
+++ teoria-comunicacion-tp/src/core/Seleccion.java
@@ -1,7 +1,10 @@
 package core;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
 
 public class Seleccion{
   
@@ -15,47 +18,102 @@ public class Seleccion{
     porcentaje = porc;
     int cantIndividuos = poblacion.getListaIndividuos().size();
     cantASeleccionar = Math.round(cantIndividuos * porcentaje / 100);
-    /** Divido por 2, ya que aplicaré dos métodos de selección, por lo que cada
-        uno seleccionará la mitad de lo necesario. */
+    /** Divido por 2, ya que aplicarï¿½ dos mï¿½todos de selecciï¿½n, por lo que cada
+        uno seleccionarï¿½ la mitad de lo necesario. */
     cantASeleccionar = cantASeleccionar / 2;
   }
 
   /**
-   * Se aplican diversos criterios de selección a la población, dejando los elementos seleccionados 
-   * en una lista interna (indivSeleccionados), para luego reemplazarla por la población.
+   * Se aplican diversos criterios de selecciï¿½n a la poblaciï¿½n, dejando los elementos seleccionados 
+   * en una lista interna (indivSeleccionados), para luego reemplazarla por la poblaciï¿½n.
    */
   public void seleccionar(){
-	  
-	  indivSeleccionados = new ArrayList<Individuo>();
-	  torneo();
-      ruleta();
-      grabarSeleccion();
-
+	  if(poblacion.getTamanioPob() > 0){	
+		  indivSeleccionados = new ArrayList<Individuo>();
+		  torneo();
+	      ruleta();
+	      grabarSeleccion();
+	  }
   }
   
   /**
-   * Método que selecciona un individuo entre dos, comparando sus aptitudes y eligiendo
+   * Mï¿½todo que selecciona un individuo entre dos, comparando sus aptitudes y eligiendo
    * el mejor.
-   * TODO: Programar el método teniendo en cuenta de utilizar solamente la interfaz Individuo.
    */
   private void torneo(){
-
+	  List<Individuo> individuos = poblacion.getListaIndividuos();
+	  float mejorAptitud = 0;
+	  
+	  if(!individuos.isEmpty()){
+		  Individuo mejor = individuos.get(1);
+		  Iterator<Individuo> it = individuos.iterator();
+		    while(it.hasNext()){ 
+			  if (mejorAptitud < mejor.getAptitud()) {
+				  mejor = (Individuo)it.next();
+			  }
+		    }
+		    
+		    this.indivSeleccionados.add(mejor);
+	  }
   }
 
 
   /**
    * https://www.google.com.ar/search?q=algoritmo+genetico+seleccion+ruleta
-   * TODO: Programar el método teniendo en cuenta de utilizar solamente la interfaz Individuo.
+   * 
+   * A cada uno de los individuos de la poblaciÃ³n se le asigna una parte proporcional a su ajuste de una ruleta, 
+   * de tal forma que la suma de todos los porcentajes sea la unidad
+   * Para seleccionar un individuo basta con generar un nÃºmero aleatorio del intervalo [0..1]
+   * Devolver el individuo situado en esa posiciÃ³n de la ruleta.
+   * Esta posiciÃ³n se suele obtener recorriendo los individuos de la poblaciÃ³n
+   * acumulando sus proporciones de ruleta hasta que la suma exceda el valor obtenido.
+   * 
    */
   private void ruleta(){
 	poblacion.recalcularAptitud();
-
+	
+	TreeMap<Float,Individuo> individuos = getListaIndividuosEnRuleta();
+	
+	//Genero una posiciÃ³n aleatoria para seleccionar al individuo.
+	Random random = new Random();
+	int posicionBuscada = random.nextInt();
+	double aptitudAcumulada = 1; /* La mayor aptitud es 1 */
+	
+	//Itero hasta obtener la aptitud acumulada y el individuo correspondiente.
+	Individuo mejor = null;	
+	for(Map.Entry<Float,Individuo> entry : individuos.entrySet()) {
+		if(posicionBuscada < aptitudAcumulada){ 
+			aptitudAcumulada += entry.getKey();
+			mejor = entry.getValue();
+		}
+	}
+	
+	 this.indivSeleccionados.add(mejor);
   }
 
   private void grabarSeleccion(){
 	  List<Individuo> individuos = poblacion.getListaIndividuos();
 	  individuos.clear();
 	  individuos.addAll(indivSeleccionados);
+  }
+  
+  /** 
+   * MÃ©todo auxiliar para generar los individuos asociados a la ruleta representada
+   * por un mapa cuya clave es la proporciÃ³n de la aptitud del individuo respecto
+   * de la aptitud total de la poblaciÃ³n a la que pertenece.
+   */
+  private TreeMap<Float,Individuo> getListaIndividuosEnRuleta(){
+	TreeMap<Float,Individuo> ruleta = new TreeMap<Float,Individuo>();	
+	List<Individuo> individuos = poblacion.getListaIndividuos();
+	
+	//Creo el mapa cuya clave es la proporcion de aptitud respecto del total de la poblaciÃ³n.
+	Iterator<Individuo> it = individuos.iterator();
+	while(it.hasNext()){
+		Individuo enRuleta = (Individuo)it.next();
+		ruleta.put(new Float(enRuleta.getAptitud() / individuos.size()), enRuleta);
+	}
+	
+	return ruleta;
   }
   
 }
